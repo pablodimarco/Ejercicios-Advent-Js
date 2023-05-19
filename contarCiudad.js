0991a19724a06dc42e7fff3fc0ed78ead55cc0d1
Pablo Di Marco <pablodimarco7@gmail.com>
2023-05-19 13:57:40 -0300

    Ejercicio Regalos Maximos

---
+++ contarCiudad.js
@@ -0,0 +1,18 @@
+const giftsCities = [12, 3, 11, 5, 7];
+const maxGifts = 20;
+const maxCities = 3;
+
+
+// la suma más alta de regalos a repartir
+// visitando un máximo de 3 ciudades
+// es de 20: [12, 3, 5]
+
+function getMaxGifts(giftsCities, maxGifts, maxCities) {
+
+
+    if(maxCities >=1 && maxGifts >= 1 && giftsCities.length >= 1){
+
+    };
+    
+};
+
